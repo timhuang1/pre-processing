3d0321ec27e1b8f4aa4c6255d4882f2889f0c867
timhuang1 <timhuangxt@gmail.com>
2022-07-08 11:39:27 +0800

    2022年 7月 8日 星期五 11时39分27秒 CST

--- examples/data_processing/split_process.sh
+++ examples/data_processing/split_process.sh
@@ -3,10 +3,10 @@ PARA_DATA_DIR=/apdcephfs/share_916081/timxthuang/paraphrase_pair_data
 
 export ln_count=2189000
 
-export data_dir=/apdcephfs/share_916081/timxthuang/paraphrase_pair_data/msmarco/div_measure
+export data_dir=/data1/paraphrase_pair_data/msmarco/div_measure
 export file_extension=".jsonl"
 export task_name=msmarco
-export part_tail="_train_high03"
+export part_tail="_train_high00"
 
 
 export corpus_file=$task_name"_sents"$part_tail$file_extension
@@ -14,7 +14,7 @@ export prefix=$task_name"_sents"$part_tail
 
 export src_file=$data_dir/$corpus_file
 export out_file=$data_dir/$prefix-part
-export nsplit=60
+export nsplit=80
 
 # export ln_count=$(wc -l < $src_file)
 
@@ -67,6 +67,9 @@ python backtrans_process.py\
   --func_name $func_name\
   --post_action "merge_to_multiple"
   # --out_filename $task_name"_sents.jsonl"\
+mv $data_dir/"post_cat_file0.jsonl" $data_dir/$prefix"_high"$file_extension
+mv $data_dir/"post_cat_file1.jsonl" $data_dir/$prefix"_low"$file_extension
+mv $data_dir/"post_cat_file2.jsonl" $data_dir/$prefix"_trah"$file_extension
 # =========
 
 # # =========
